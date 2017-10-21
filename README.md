a822bc75b79ca86785575e93303c8e9a647765f2
stunstun <wjdsupj@gmail.com>
2017-10-21 19:02:12 +0900

    Initialize project

---
+++ README.md
@@ -0,0 +1,26 @@
+
+## Papago with ES6
+
+#### I did
+
+```
+$ npm init
+$ mkdir lib test
+$ touch .babelrc
+$ touch .gitignore
+```
+
+```
+$ npm install --save-dev babel babel-cli babel-core babel-preset-es2015 babel-register mocha chai
+```
+
+#### You should
+
+```
+$ npm install -g babel-cli
+$ npm install
+```
+
+```
+$ npm test
+```
---
+++ test/response.spec.js
@@ -0,0 +1,30 @@
+import { assert } from 'chai';
+import response, { successCode } from '../lib/response';
+
+describe('response', () => {
+    before(() => {
+
+    });
+
+    it('parse json', () => {
+        const body = {
+            message: { 
+                result: { 
+                    translatedText: "Hello", 
+                    srcLangType: "ko",
+                }
+            }
+        };
+        const result = response(body);
+        assert.equal(result.code, successCode);
+    });
+
+    it('parse failed json', () => {
+        const body = {
+            errorCode: 405, 
+            errorMessage: "Not Found Resources",
+        };
+        const result = response(body);
+        assert.notEqual(result.code, successCode);
+    });
+});
